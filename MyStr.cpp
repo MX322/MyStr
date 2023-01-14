d31d2275ad6c62610c9daacb4f7a4108db49ccd3
MX <105460091+MyNamEdoNtLike@users.noreply.github.com>
2023-01-14 09:23:04 +0200

    master

--- MyStr.cpp
+++ MyStr.cpp
@@ -25,6 +25,12 @@ MyStr::MyStr(const char* str)
 	counter++;
 }
 
+MyStr::MyStr(MyStr& obj)
+{
+	this->size = obj.size;
+	this->str = new char[size];
+	strcpy_s(this->str, size, obj.str);
+}
 
 MyStr::~MyStr()
 {
@@ -52,9 +58,126 @@ void MyStr::ShowCounterObj()
 }
 
 
-int main()
+void MyStr::MyStrCpy(MyStr& obj)
 {
-	MyStr MyString = "Some text...";
-	MyString.Output();
-	MyStr::ShowCounterObj();
+	this->size = obj.size;
+	this->str = new char[size];
+	for (int i = 0; i < size; i++)
+	{
+		str[i] = obj.str[i];
+	}
 }
+
+
+bool MyStr::MyStrStr(const char* str)
+{
+	int j = 0;
+	int end = strlen(str);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (!end)
+		{
+			return true;
+		}
+
+		if (this->str[i] == str[j])
+		{
+			j++;
+			end--;
+		}
+	}
+
+	return false;
+}
+
+
+int MyStr::MyChr(char c)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (str[i] == c)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+
+int MyStr::MyStrLen()
+{
+	return size;
+}
+
+
+void MyStr::MyStrCat(MyStr& obj)
+{
+	int newSize = this->size + obj.size + 1;
+	char* newStr = new char[newSize];
+
+	int j = 0;
+	for (int i = 0; i < this->size; i++, j++)
+	{
+		newStr[j] = this->str[i];
+	}
+
+	j--;
+
+	for (int i = 0; i < obj.size; i++, j++)
+	{
+		newStr[j] = obj.str[i];
+	}
+
+	this->size = newSize;
+	this->str = newStr;
+
+}
+
+
+void MyStr::MyDelChr(char c)
+{
+
+	char* newStr = new char[size];
+
+	int end = size;
+	int j = 0;
+	for (int i = 0; i < end; i++)
+	{
+		if (str[i] != c)
+		{
+			newStr[j] = str[i];
+			j++;
+		}
+
+		else
+		{
+			size--;
+		}
+
+	}
+
+	
+	str = newStr;
+}
+
+
+int  MyStr::MyStrCmp(MyStr& obj)
+{
+	if (this->size > obj.size)
+	{
+		return 1;
+	}
+
+	else if (this->size < obj.size)
+	{
+		return -1;
+	}
+
+	else if (this->size == obj.size)
+	{
+		return 0;
+	}
+}
+
--- MyStr.h
+++ MyStr.h
@@ -10,11 +10,22 @@ public:
 	MyStr();
 	MyStr(int size);
 	MyStr(const char* str);
+	MyStr(MyStr& obj);
 	~MyStr();
 
 	void Input();
 	void Output();
 	static void ShowCounterObj();
+	void MyStrCpy(MyStr& obj);// копирование строк
+	bool MyStrStr(const char* str);// поиск подстроки в строке
+	int MyChr(char c);//поиск символа в строке(индекс найденного символа, либо -1)
+	int MyStrLen();// возвращает длину строки
+	void MyStrCat(MyStr& obj); // объединение строк
+	void MyDelChr(char c); // удаляет указанный символ 
+	int MyStrCmp(MyStr& obj); // сравнение строк ( -1  первая строка меньше чем вторая   |   1   первая больше чем вторая   |   0   строки равны!)
+	
+		
+
 
 };
 
